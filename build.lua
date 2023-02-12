7cb4cb911dce967b7cbe6ec2565bdc0357fa081e
gusbrs <19410606+gusbrs@users.noreply.github.com>
2023-02-12 15:41:10 -0300

    Don't wrap lines in test logs

--- build.lua
+++ build.lua
@@ -10,6 +10,10 @@ typesetfiles = {"*.tex"}
 -- Three runs
 checkruns = 3
 
+-- Don't wrap/truncate lines in test logs
+-- See https://tex.stackexchange.com/q/674844#comment1676566_674846
+maxprintline = 1000
+
 -- Use dev formats for regression tests
 -- See https://tex.stackexchange.com/q/611424
 checkengines = {"pdftex","luatex","xetex","pdftexdev","luatexdev","xetexdev"}
